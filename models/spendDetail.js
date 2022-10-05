4b51ee4d36d6266c607ddc2883e6498c116e4783
Shireen <shireen@koskii.com>
2022-10-05 20:48:15 +0530

    task -2: Completed backend database integeration with mongodb atlas

---
+++ models/spendDetail.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+
+const yourSchema = new mongoose.Schema({
+    thread_id: { type: String },
+    email_subject: { type: String },
+    spent_date: { type: Date}
+  });
+
+  const spendDetail = mongoose.model("spend_detail",yourSchema);
+
+  module.exports = spendDetail
---
+++ mongodbConfig.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose')
+const api_constants = require('./api_constants')
+
+module.exports.db_connection= function(){
+
+    const connection =  mongoose.connect(api_constants.db_url);
+    console.log('Db connection established successfully!')
+   return connection
+}
+
+// console.log(SomeModel.find({_id: "633d5812dd1b966c6eea3aee"}) )
+
+//     SomeModel.find().then(customers => { 
+//       console.log("insiden ")             
+//     console.log(customers[0].thread_id); // 'A'
+//     console.log(customers[3].thread_id); 
+//     customers.map(customer=>{console.log(customer.thread_id)})
+//   })
