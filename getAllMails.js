315381be2a70b977ee729e749574eb5a0dd6dbdb
Shireen <shireen@koskii.com>
2022-10-13 10:21:53 +0530

    Task -3: automated refresh token generation for GoogleApi

--- getAllMails.js
+++ getAllMails.js
@@ -1,22 +1,29 @@
 const api_helper = require('./api_constants')
 const request_call = require('./request_call')
 const getSpends = require('./getSpendDetails')
+const getToken = require('./getGoogleAccessToken')
+
 module.exports.mails_yesterday = function(){
 
  getsAllMail = api_helper.gets_all_mail.concat(api_helper.yesterday)
- result = request_call('GET',getsAllMail,'')
-
- result.then(function(response){
-    messages = response.data.messages
-    console.log(messages)
-    messages.map(messageId =>{
-        console.log(messageId.id)
-        getSpends.getThreadDetails(messageId.id)
-    })
-
-  }).catch(function(error){
-    console.log(error.response.data)
-  })
+ console.log('before the call access token'+getToken.access_token())
+ getToken.access_token().then(function(){
+  result = request_call('GET',getsAllMail,'',{ Authorization: "Bearer " + accessToken})
+  result.then(function(response){
+     messages = response.data.messages
+     console.log(messages)
+     messages.map(messageId =>{
+         console.log(messageId.id)
+         getSpends.getThreadDetails(messageId.id)
+     })
+ 
+   }).catch(function(error){
+     console.log(error.response.data)
+   })
+ }).catch((error)=>{
+  console.log("Please check your access token generation process")
+ })
+ 
 
 }
 
--- request_call.js
+++ request_call.js
@@ -2,14 +2,14 @@ const axios = require("axios");
 const { response } = require("express");
 const api_helper = require("./api_constants");
 
-module.exports = function (method, uri, body) {
+module.exports = function (method, uri, body,headers) {
   
   let config = {
     method: method.toLowerCase(),
     url: uri,
-    data: body,
-    headers: { Authorization: "Bearer " + api_helper.access_token },
+    headers: headers,
   };
+  console.log(config)
 
    apiResult = axios(config)
 
