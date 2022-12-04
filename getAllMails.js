ee3aa3ba20df2674eb4a9d3bea6fb29f4822cb9d
Shireen <shireen@koskii.com>
2022-12-04 16:27:14 +0530

    modified the save method to create and update method

--- getAllMails.js
+++ getAllMails.js
@@ -2,13 +2,14 @@ const api_helper = require('./api_constants')
 const request_call = require('./request_call')
 const getSpends = require('./getSpendDetails')
 const getToken = require('./getGoogleAccessToken')
-const { yesterday } = require('./api_constants')
 
 module.exports.mails_yesterday = async function(){
 try {
  accessToken = await getToken.access_token();
  for(category of api_helper.array_of_categories){
- getsAllMail =`${api_helper.gets_all_mail} after:${yesterday} before:${moment(yesterday, 'YYYY/MM/DD').add(1, 'days').format( 'YYYY/MM/DD')} label:${category}`
+//  getsAllMail =`${api_helper.gets_all_mail} after:${apiDate} before:${moment(apiDate, 'YYYY/MM/DD').add(1, 'days').format( 'YYYY/MM/DD')} label:${category}`
+getsAllMail =`${api_helper.gets_all_mail} ${api_helper.yesterday} label:${category}`
+
  console.log(`Request being made is ${getsAllMail}`)
   response = await request_call('GET',getsAllMail,'',{ Authorization: "Bearer " + accessToken})
   console.log(response.data)
@@ -17,6 +18,7 @@ try {
   if (numberOfRecords>0){
     messagesId = response.data.messages
     messagesId.map(message=>{
+      console.log(message.threadId)
       getSpends.getThreadDetails(message.threadId, category)
     })
   }
--- getSpendDetails.js
+++ getSpendDetails.js
@@ -3,19 +3,25 @@ const request_call = require("./request_call")
 const spendDetail = require('./models/spendDetail')
 const getToken = require('./getGoogleAccessToken')
 
-module.exports.getThreadDetails=function(threadId, category){
+module.exports.getThreadDetails= function(threadId, category){
 
   getToken.access_token().then(function(){
 
   emailDetail = request_call('GET',api_constants.get_mail_details_url+threadId, '',{ Authorization: "Bearer " + accessToken})
 
-  emailDetail.then(function(response){
+  emailDetail.then(async function(response){
     headersArray= response.data.payload.headers
     foundSubject = headersArray.find(nameValue => nameValue.name === 'Subject')
     amount_spent =foundSubject.value.replace(/\D/g, '') 
     console.log(foundSubject.value.replace(/\D/g, ''))
-    spendDetail.create({thread_id: threadId, email_subject: foundSubject.value, spent_date: api_constants.spent_date, money_spent: amount_spent, category: category})
-     console.log(`category is ${category}, amount spent is ${amount_spent}`)
+    filter = {thread_id: threadId}
+    update = {email_subject: foundSubject.value, spent_date: api_constants.yesterday, money_spent: amount_spent, category: category}
+    await spendDetail.findOneAndUpdate(filter, update, {
+      new: true,
+      upsert: true // Make this update into an upsert
+    });
+
+    console.log(`category is ${category}, amount spent is ${amount_spent}`)
   }).catch(function(error){
     console.log('error'+error)
   })
